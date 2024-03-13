f6f4a107cc4862540620de87695fa5035aac0964
rhenzo <rhenzo@rhenzoDebian.168.0.119>
2024-03-13 18:18:00 -0300

    Relatorio 2 - atividade 2

--- Relatorio-02/Exercicio_1.m
+++ Relatorio-02/Exercicio_1.m
@@ -22,7 +22,7 @@ f = [-fs/2:passo_f:fs/2];
 m_t = Am * cos(2*pi*fm*t);
 c_t = Ac * cos(2*pi*fc*t);
 
-r_t = m_t .* c_t;
+s_t = m_t .* c_t;
 r_t = r_t .* c_t;
 
 am_dsb_tc_t = Am.*(1 + cos(2*pi*fm*t)).*Ac.*cos(2*pi*fc*t);
@@ -38,10 +38,16 @@ am_dsb_sc_f = fftshift(am_dsb_sc_f);
 ordem = 80;
 f_cut = 1e3;
 filtro_pb_t = fir1(ordem,(f_cut*2)/fs);
-%am_dsb_tc_f_filtrado =
+
+
+ordem = 80;
+f_cut = 1e3;
+filtro_pb_t = fir1(ordem,(f_cut*2)/fs);
+am_dsb_tc_f_filtrado =
 
 
 am_dsb_sc_t_filtrado = filter(filtro_pb_t,1,am_dsb_sc_t);
+am_dsb_sc_f_filtrado = fftshift(fft(am_dsb_sc_t_filtrado ))/ length(am_dsb_sc_t_filtrado );
 
 figure(1)
 subplot(411)
@@ -76,8 +82,11 @@ xlim([-3*fc 3*fc])
 figure(3)
 freqz(filtro_pb_t)
 
-figure(3)
+figure(4)
 subplot(211)
+plot(t,am_dsb_sc_t_filtrado,"k")
+xlim([0 3*T])
+subplot(212)
 plot(f,am_dsb_sc_f_filtrado)
 xlim([-3*fc 3*fc])
 
---
+++ aula130324.m
@@ -0,0 +1,97 @@
+pkg load signal;
+clear all;
+close all;
+clc;
+
+[sinal1, Fs] = audioread('audio-1.wav');
+fcc = 10e3;
+Ac = 1 ;
+duracao = length(sinal1)/Fs;
+Ts = 1/Fs;
+t = [0:Ts:duracao-Ts];
+
+[sinal2, Fs] = audioread('audio-2.wav');
+
+
+sinal1 = transpose(sinal1);
+sinal2 = transpose(sinal2);
+sinal2 = sinal2(1:length(sinal1));
+
+
+
+c1_t = Ac*cos(2*pi*fcc*t);
+c2_t = -Ac*sin(2*pi*fcc*t);
+
+sinal1_f = fftshift(fft(sinal1)/length(sinal1));
+sinal2_f = fftshift(fft(sinal2)/length(sinal2));
+
+
+s1_t = sinal1 .* c1_t;
+s2_t = sinal2 .* c2_t;
+
+
+passo_f = 1/duracao;
+f = [-Fs/2:passo_f:Fs/2 - passo_f];
+
+
+s1_f = fftshift(fft(s1_t)/length(s1_t));
+s2_f = fftshift(fft(s2_t)/length(s2_t));
+
+y_t = s1_t + s2_t;
+y_f = fftshift(fft(y_t)/length(y_t));
+
+audiowrite('audio-y.wav',y_t,Fs);
+
+sa_t = y_t .* c1_t;
+sb_t = y_t .* c2_t;
+
+sa_f = fftshift(fft(sa_t)/length(sa_t));
+sb_f = fftshift(fft(sb_t)/length(sb_t));
+
+filtro_pb = [zeros(1, (length(f) - 2*Fs)/2 ) ones(1,2*Fs) zeros(1, (length(f) - 2*Fs)/2)];
+
+ya_f = sa_f .* filtro_pb;
+yb_f = sb_f .* filtro_pb;
+
+ya_t = ifft(ifftshift(ya_f)) * length(sa_f);
+yb_t = ifft(ifftshift(yb_f)) * length(sa_f);
+
+audiowrite('audio-a.wav',ya_t,Fs);
+audiowrite('audio-b.wav',yb_t,Fs);
+
+
+figure(1)
+subplot(211);
+plot(t,sinal1);
+
+subplot(212);
+plot(t,sinal2);
+
+figure(2)
+subplot(411);
+plot(f,abs(sinal1_f));
+subplot(412);
+plot(f,abs(s1_f));
+
+subplot(413);
+plot(f,abs(sinal2_f));
+subplot(414);
+plot(f,abs(s2_f));
+
+figure(3)
+subplot(211)
+plot(t,y_t);
+subplot(212)
+plot(f,abs(y_f));
+
+figure(4)
+subplot(211);
+plot(f,abs(sa_f));
+subplot(212);
+plot(f,abs(sb_f));
+
+figure(5)
+subplot(211);
+plot(f,abs(ya_f));
+subplot(212);
+plot(f,abs(yb_f));
