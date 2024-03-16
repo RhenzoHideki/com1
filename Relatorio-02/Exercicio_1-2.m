e9fc0f483ddbdcbb9bf2420b7e6a3cf2a17eb57a
rhenzo <rhenzo@rhenzoDebian.168.0.119>
2024-03-16 11:33:54 -0300

    relatorio 2

--- Relatorio-02/Exercicio_1-2.m
+++ Relatorio-02/Exercicio_1-2.m
@@ -1,63 +1,77 @@
-pkg load signal
-close all;
-clear all;
-clc;
+clear all; close all; clc;
+pkg load signal;
 
-Am = 1;
-Ac = 1;
+fs = 1e5;
+ts = 1/fs;
 
-fm = 1e3;
-fc = 10e3;
+f1 = 1e3;
+A1 = 3;
 
-fs = 10*fc;
-Ts = 1/fs;
-t_final = 2;
-T = 1/fm;
+f2 = 10e3;
+A2 = 1;
 
-t = [0:Ts:t_final];
 
-passo_f = 1/t_final;
-f = [-fs/2:passo_f:fs/2];
-
-m_t = Am * cos(2*pi*fm*t);
-c_t = Ac * cos(2*pi*fc*t);
-
+t = [0 : ts : 1];
+m_t = A1 * cos(2*pi*f1*t);
+c_t = A2 * cos(2*pi*f2*t);
 s_t = m_t .* c_t;
 r_t = s_t .* c_t;
 
-dsb_tc = Ac *(1 + m_t) .* cos(2*pi*fc*t);
+passo_f = 1;
+f = [-fs/2 : passo_f : fs/2];
+  M_f = fftshift(fft(m_t) / length(m_t));
+  C_f = fftshift(fft(c_t) / length(c_t));
+  S_f = fftshift(fft(s_t) / length(s_t));
+  R_f = fftshift(fft(r_t) / length(r_t));
+
+ordem_filtro = 10; % Ordem do filtro FIR
+coeficientes_filtro = fir1(ordem_filtro, [1000 3000] * 2/fs); % Projeta o filtro passa-baixa
+
+m_t_hat = filter(coeficientes_filtro, 1, r_t); % Filtra o sinal modulado para demodulação
+m_f_hat = fftshift(fft(m_t_hat) / length(m_t_hat));
+
+figure(1)
+subplot(4, 1, 1)
+plot(t, m_t)
+xlim([0 5*(1/f1)])
+title('Sinal Modulante')
+
+subplot(4, 1, 2)
+plot(t, c_t)
+xlim([0 5*(1/f1)])
+title('Portadora')
+
+subplot(4, 1, 3)
+plot(t, r_t)
+xlim([0 5*(1/f1)])
+title('Sinal Modulado')
+
+subplot(4, 1, 4)
+plot(t, m_t_hat)
+xlim([0 5*(1/f1)])
+title('Sinal Demodulado')
+xlabel('Tempo (s)')
 
 
+figure(2)
+subplot(4, 1, 1)
+plot(f, abs(M_f))
+xlim([-10000 10000])
+title('Sinal Modulante')
 
+subplot(4, 1, 2)
+plot(f, abs(C_f))
+xlim([-100000 100000])
+title('Portadora')
 
-subplot(411);
-plot(t, m_t);
-title('Message Signal');
-xlabel('Time');
-ylabel('Amplitude');
-xlim([0 3*T])
+subplot(4, 1, 3)
+plot(f, abs(R_f))
+xlim([-10000 10000])
+title('Sinal Modulado')
 
+subplot(4, 1, 4)
+plot(f, abs(m_f_hat))
+xlim([-10000 10000])
+title('Sinal Demodulado')
 
-subplot(412);
-plot(t, c_t);
-title('Carrier Signal');
-xlabel('Time');
-ylabel('Amplitude');
-xlim([0 3*T])
-
-
-subplot(413);
-plot(t, dsb_tc);
-title('DSB-TC Modulated Signal');
-xlabel('Time');
-ylabel('Amplitude');
-xlim([0 3*T])
-
-
-subplot(414);
-plot(t, s_t);
-title('DSB-SC Modulated Signal');
-xlabel('Time');
-ylabel('Amplitude');
-xlim([0 3*T])
-
+xlabel('Tempo (s)')
--- Relatorio-02/Exercicio_1.m
+++ Relatorio-02/Exercicio_1.m
@@ -7,7 +7,7 @@ Am = 1;
 Ac = 1;
 
 fm = 1e3;
-fc = 10e3;
+fc = 30e3;
 
 fs = 10*fc;
 Ts = 1/fs;
@@ -19,34 +19,18 @@ t = [0:Ts:t_final];
 passo_f = 1/t_final;
 f = [-fs/2:passo_f:fs/2];
 
-m_t = Am * cos(2*pi*fm*t);
-c_t = Ac * cos(2*pi*fc*t);
+m_t = Am * cos(2*pi*fm*t); % Sinal
+c_t = Ac * cos(2*pi*fc*t); % Portadora
 
-s_t = m_t .* c_t;
-%r_t = r_t .* c_t;
+%DSB-SC
+rc_t = m_t .* c_t;
+sc_t = rc_t .* c_t;
+%sc_t = Ac.*cos(2*pi*fc*t).*m_t;
 
-am_dsb_tc_t = Am.*(1 + cos(2*pi*fm*t)).*Ac.*cos(2*pi*fc*t);
-am_dsb_sc_t = Ac.*cos(2*pi*fc*t).*m_t;
+%DSB-TC
+tc_t = Am.*(1 + cos(2*pi*fm*t)).*Ac.*cos(2*pi*fc*t);
 
-am_dsb_tc_f = fft(am_dsb_tc_t)/length(am_dsb_tc_t);
-am_dsb_tc_f = fftshift(am_dsb_tc_f);
-
-am_dsb_sc_f = fft(am_dsb_sc_t)/length(am_dsb_sc_t);
-am_dsb_sc_f = fftshift(am_dsb_sc_f);
-
-
-ordem = 80;
-f_cut = 1e3;
-filtro_pb_t = fir1(ordem,(f_cut*2)/fs);
-
-
-ordem = 80;
-f_cut = 1e3;
-filtro_pb_t = fir1(ordem,(f_cut*2)/fs);
-
-am_dsb_sc_t_filtrado = filter(filtro_pb_t,1,am_dsb_sc_t);
-am_dsb_sc_f_filtrado = fftshift(fft(am_dsb_sc_t_filtrado ))/ length(am_dsb_sc_t_filtrado );
-
+%Plot 1 - PLOT DOS SINAIS -------------------------------------------------------------------------------------------------------
 figure(1)
 subplot(411)
 plot(t,m_t,"r")
@@ -59,32 +43,61 @@ xlim([0 3*T])
 title("c(t)")
 
 subplot(413)
-plot(t,am_dsb_tc_t,"k")
+plot(t,tc_t,"k")
 xlim([0 3*T])
 title("am-dsb-tc (t)")
 
 subplot(414)
-plot(t,am_dsb_sc_t,"b")
+plot(t,sc_t,"b")
 xlim([0 3*T])
 title("am-dsb-sc (t)")
 
+%--------------------------------------------------------------------------------------------------------------------------------
+% Mudança de dominio para frequência
+M_f = fftshift(fft(m_t)/length(m_t));
+C_f = fftshift(fft(c_t) / length(c_t));
+TC_f = fftshift(fft(tc_t) / length(tc_t));
+SC_f = fftshift(fft(sc_t) / length(sc_t));
+%Plot 1 - PLOT DOS SINAIS -------------------------------------------------------------------------------------------------------
 figure(2)
-subplot(211)
-plot(f,am_dsb_tc_f)
-xlim([-3*fc 3*fc])
+subplot(411)
+plot(f,abs(M_f),"r")
+xlim([-fs fs])
+title("M(f)")
 
-subplot(212)
-plot(f,am_dsb_sc_f)
-xlim([-3*fc 3*fc])
+subplot(412)
+plot(f,abs(C_f),"g")
+xlim([-fs fs])
+title("C(f)")
+
+subplot(413)
+plot(f,abs(TC_f),"k")
+xlim([-fs fs])
+title("am-dsb-tc (f)")
+
+subplot(414)
+plot(f,abs(SC_f),"b")
+xlim([-fs fs])
+title("am-dsb-sc (f)")
+
+%--------------------------------------------------------------------------------------------------------------------------------
+
+
+
+ordem = 80;
+f_cut = 1e3;
+filtro_pb_t = fir1(ordem,(f_cut*2)/fs);
+
+sc_t_filtrado = filter(filtro_pb_t,1,sc_t);
+sc_f_filtrado = fftshift(fft(sc_t_filtrado ))/ length(sc_t_filtrado );
 
 figure(3)
-freqz(filtro_pb_t)
-
-figure(4)
 subplot(211)
-plot(t,am_dsb_sc_t_filtrado,"k")
+plot(t,sc_t_filtrado,"b")
 xlim([0 3*T])
+title("am-dsb-sc (t)")
+
 subplot(212)
-plot(f,am_dsb_sc_f_filtrado)
-xlim([-3*fc 3*fc])
+plot(f,abs(sc_t_filtrado));
+xlim([-fs fs])
 
